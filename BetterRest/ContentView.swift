1a9e8e97de95f3d33c6e24b01e8f596da88fbb68
Matt Sutton <mattsuttondev@gmail.com>
2020-01-29 12:48:26 -0500

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Matthew Sutton on 1/29/20.
+//  Copyright © 2020 Matthew Sutton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
